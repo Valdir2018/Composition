a86b0b342ee7a785125c7834f4bed3c0dbe1989e
Valdir Silva <valdir.correia@flexcontact.com.br>
2019-02-19 15:03:11 -0300

    Classes criadas, Worker, workerLevel, HourContract

---
+++ Course-composicao/Entities/HourContact.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course_composicao.Entities
+{
+    class HourContact
+    {
+        public DateTime Date { get; set; }
+        public double ValuePerHour  { get; set; }
+        public int Hours { get; set; }
+
+        public HourContact()
+        {
+            // Construtor padrao
+        }
+
+        public HourContact(DateTime date, double valuePerHour, int hours)
+        {
+            Date = date;
+            ValuePerHour = valuePerHour;
+            Hours = hours;
+        }
+
+        public double TotalValue()
+        {
+            return Hours * ValuePerHour;
+        }
+    }
+}
---
+++ Course-composicao/Entities/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Course_composicao.Entities.Enums;
+
+namespace Course_composicao.Entities
+{
+    class Worker
+    {
+        public string Name { get; set; }
+        public WorkerLevel level { get; set; }
+        public double baseSalary { get; set; }
+    }
+}
