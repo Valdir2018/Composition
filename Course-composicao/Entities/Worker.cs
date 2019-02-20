e49e7de1da467169cb53633de2d2e3c6ac5a9e97
Valdir Silva <valdir.correia@flexcontact.com.br>
2019-02-20 19:34:48 -0300

    Regras de negocio criada - Composicao entre objetos instanciada

--- Course-composicao/Entities/Worker.cs
+++ Course-composicao/Entities/Worker.cs
@@ -11,6 +11,44 @@ namespace Course_composicao.Entities
     {
         public string Name { get; set; }
         public WorkerLevel level { get; set; }
-        public double baseSalary { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; } // Associação entre classes = Composição entre objetos
+        public List<HourContact> Contracts { get; set; } = new List<HourContact>(); // Lista de contrato
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, WorkerLevel level, double baseSalary, Department department)
+        {
+            Name = name;
+            this.level = level;
+            this.BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddContract(HourContact contact)
+        {
+            Contracts.Add(contact); // Adiciona um contrato na lista
+        }
+        public void RemoveContract(HourContact contact)
+        {
+            Contracts.Remove(contact); // Remove um contrato
+        }
+
+        public double Income(int year, int month) // Recebe como argumento o ano e mês
+        {
+            double sum = BaseSalary;
+            foreach(HourContact contract in Contracts)
+            {
+                if (contract.Date.Year == year && contract.Date.Month == month)
+                {
+                    sum += contract.TotalValue();
+                }
+              
+            }
+            return sum;
+        }
     }
 }
--- Course-composicao/Program.cs
+++ Course-composicao/Program.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Course_composicao.Entities.Enums;
+using Course_composicao.Entities;
+using System.Globalization;
 
 namespace Course_composicao
 {
@@ -10,6 +13,17 @@ namespace Course_composicao
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter department´s name: ");
+            string deptName = Console.ReadLine();
+            Console.Write("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("level (JUnior/Midlevel/Senior): ");
+            WorkerLevel level;
+            Enum.TryParse("WorkerLevel", true, out level).(Console.ReadLine());
+
+            Console.Write("Base Salary: ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
         }
     }
